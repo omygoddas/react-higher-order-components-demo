15539ce980c48074e86d5221d1683f81c6f9c286
ChihCheng Lai <omygoddas@gmail.com>
2020-06-13 18:07:07 +0800

    Add user-list and user-profile components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import UserProfile from './components/user-profile/user-profile.component';
+import UserList from './components/user-list/user-list.component';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserList />
+      <UserProfile name="Skywalker" email="Skywalker1234@qmail.com" />
     </div>
   );
 }
---
+++ src/components/user-profile/user-profile.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data.slice(0, 3) }));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.email}</h2>
+        Posts:
+        {this.state.posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
